4f3fd6167484b0495218dad2dbac3067258b2d20
Yohann2207 <yoyofbi2a@gmail.com>
2024-12-12 14:32:14 +0100

    modif

--- Backend/app.py
+++ Backend/app.py
@@ -1,11 +1,20 @@
 from flask import Flask, request, jsonify
 from api.routes import api_bp  # Blueprint des routes API
+import os
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return "Bonjour, Flask !"
+
+
+@app.route('/get-paths', methods=['POST'])
+def get_paths():
+    data = request.get_json(force=True)
+    paths = data.get('paths', [])
+    return jsonify({'paths': paths}), 200
+
 
 # Configuration (Clés API, chemins, etc.)
 app.config.from_object("config")
