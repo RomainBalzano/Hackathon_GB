72fb1eb12302dc7bfb754c4d42694d0d5bd0351d
ROMAIN BALZANO <20180770@webmail.universita.corsica>
2024-12-12 14:58:58 +0100

    add Cors

--- Backend/app.py
+++ Backend/app.py
@@ -1,8 +1,10 @@
 from flask import Flask, request, jsonify
 from api.routes import api_bp  # Blueprint des routes API
+from flask_cors import CORS
 import os
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello():
@@ -14,6 +16,16 @@ def get_paths():
     data = request.get_json(force=True)
     paths = data.get('paths', [])
     return jsonify({'paths': paths}), 200
+
+@app.route('/Image', methods=['POST'])
+def receive_paths():
+    try:
+        data = request.get_json(force=True)
+        if not data or 'paths' not in data:
+            return jsonify({"error": "Invalid input, 'paths' key is required"}), 400
+        return jsonify({"data": data}), 200
+    except Exception as e:
+        return jsonify({"error": str(e)}), 400
 
 
 # Configuration (Clés API, chemins, etc.)
