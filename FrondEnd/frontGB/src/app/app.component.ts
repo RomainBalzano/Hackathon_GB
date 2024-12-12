037e11ecb80919376a5a7fc8be116eeea6664734
liviagr <113026321+liviagr@users.noreply.github.com>
2024-12-12 11:36:58 +0100

    feat(app-front): header

--- FrondEnd/frontGB/src/app/app.component.ts
+++ FrondEnd/frontGB/src/app/app.component.ts
@@ -1,16 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import {CardComponent } from './card/card.component';
-
-
+import { CardComponent } from './card/card.component';
+import { HeaderComponent } from './header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, CardComponent, HeaderComponent],
 
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'frontGB';
